2f0031e7e1b39ad906f80ef74548ee61bdca69fa
muhammadfahadfarooq <fahad.farooq7013@gmail.com>
2024-04-30 08:39:51 +0500

    init

---
+++ index.js
@@ -0,0 +1,23 @@
+import inquirer from "inquirer";
+let todos = [];
+let condition = true;
+while (condition) {
+    let addTask = await inquirer.prompt([
+        {
+            name: 'todo',
+            type: 'input',
+            message: 'What you want to add in your Todos?'
+        }
+    ]);
+    todos.push(addTask.todo);
+    console.log(todos);
+    let addCondTask = await inquirer.prompt([
+        {
+            name: 'addMore',
+            type: 'confirm',
+            message: 'Do you want to add more?',
+            default: 'false',
+        }
+    ]);
+    condition = addCondTask.addMore;
+}
